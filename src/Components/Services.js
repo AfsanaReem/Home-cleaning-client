78f15142ce2d5351e6a8bc987ae45c523ed31712
Reem <apple@Apples-MacBook-Pro.local>
2022-11-10 06:47:03 +0600

    private route added

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import ServiceTab from './ServiceTab';
+import useTitle from './useTitle';
 
 const Services = () => {
+    useTitle('Services')
     const [services, setServices] = useState([]);
     useEffect(() => {
         fetch('http://localhost:5000/services')
